2b116a9e9abd9f8bf8fa6680fcfe0ca5d1cbca60
ArthurDelaunay <delaunayfredericarthur@gmail.com>
2022-06-22 11:44:52 +0200

    exercice 1 à 6

---
+++ index.js
@@ -0,0 +1,43 @@
+// 01 - Fruits
+// const fruits = ["mango","lemon","blueberry"]
+// console.log(fruits)
+// console.table(fruits)
+
+// 02 - Access
+// const ingredients = ["eggs","milk","butter"]
+// console.log(ingredients[ingredients.indexOf("milk")])
+
+// 03 - Add and Remove
+// const objects = ["pen","book","lamp"]
+// console.log(objects)
+// objects.unshift("chair")
+// console.log(objects)
+// objects.pop()
+// console.log(objects)
+// objects.push("laptop")
+// console.log(objects)
+// objects.shift()
+// console.log(objects)
+
+// 04 - Order
+// const numbers = [4, 10, 8, 12, 6]
+// numbers.reverse()
+// console.log(numbers)
+
+// 05 - Boucle
+// let total = 0
+// const limit = 10
+
+// for (let i = 0; i <= limit; i += 1){
+//     total += i
+// }
+// console.log(total)
+
+// 06 - Reverse
+// const sentence = "Hello Konexio !"
+// let reverdedSentence = ""
+
+// for (i=0; i <= sentence.length; i += 1){
+//     reverdedSentence = reverdedSentence.concat(sentence.charAt(sentence.length - i))
+// }
+// console.log(reverdedSentence)
